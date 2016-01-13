0f52452beefbcd03c1c02f1a1063ca6db2ac3ba9
Hussen Fakhrulddin <hf556482@gmail.com>
2016-01-12 20:50:41 -0800

    Added the timer

--- 01-AgeCalculator/01-AgeCalculator/Program.cs
+++ 01-AgeCalculator/01-AgeCalculator/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace _01_AgeCalculator
 {
@@ -11,79 +12,94 @@ namespace _01_AgeCalculator
         static void Main(string[] args)
         {
             try {
-                Console.WriteLine("What is your birthday ? (Enter date in format MM/DD/YYYY): ");
-                String inPut = Console.ReadLine();
-                DateTime inPut2 = DateTime.Parse(inPut);
-                DateTime Now = DateTime.Now;
-                int birthYear = inPut2.Year;
-                int todayYear = Now.Year;
-                int yearResults = todayYear - birthYear;
 
-
-                int birthMonth = inPut2.Month;
-                int todayMonth = DateTime.Now.Month;
-                int monthResults = todayMonth - birthMonth;
-
-                int birthDay = inPut2.Day;
-                int todayDay = DateTime.Now.Day;
-                int daysResults = todayMonth -birthMonth;
-
-
-                if (birthMonth > todayMonth)
+                bool enterNewData = false;
+                while (enterNewData == false)
                 {
-                    monthResults = 12 - (birthMonth - todayMonth);
-                    yearResults = yearResults - 1;
+                    Console.WriteLine("What is your birthday ? (Enter date in format MM/DD/YYYY): ");
+                    String inPut = Console.ReadLine();
+                    DateTime inPut2 = DateTime.Parse(inPut);
+                    DateTime Now = DateTime.Now;
+                    int birthYear = inPut2.Year;
+                    int todayYear = Now.Year;
+                    int yearResults = todayYear - birthYear;
+
+
+                    int birthMonth = inPut2.Month;
+                    int todayMonth = DateTime.Now.Month;
+                    int monthResults = todayMonth - birthMonth;
+
+                    int birthDay = inPut2.Day;
+                    int todayDay = DateTime.Now.Day;
+                    int daysResults = todayMonth - birthMonth;
+
+
+                    if (birthMonth > todayMonth)
+                    {
+                        monthResults = 12 - (birthMonth - todayMonth);
+                        yearResults = yearResults - 1;
+                    }
+                    else
+                    {
+                        monthResults = todayMonth - birthMonth;
+                    };
+
+
+
+                    if (birthDay > todayDay)
+                    {
+                        daysResults = 30 - (birthDay - todayDay);
+                        monthResults = monthResults - 1;
+                    }
+                    else
+                    {
+                        daysResults = todayDay - birthDay;
+                    };
+
+
+
+                    int weeksFromDays = daysResults / 7;
+                    int daysFromDays = daysResults % 7;
+
+
+
+
+
+                    double daysInMonths = monthResults * 30.41;
+                    double leepDays = yearResults / 4;
+                    double daysInMonth28 = daysInMonths + leepDays;
+
+                    double reminingDaysinweeks = Math.Floor(daysInMonth28 % 7);
+                    double daysResults3 = reminingDaysinweeks + daysFromDays;
+                    double weeksInMonths = Math.Floor((daysInMonth28) / 7 + weeksFromDays);
+
+                    bool exitCurrentSession = false;
+                    while (exitCurrentSession == false) {
+
+
+                        for (int i = 0; i < 5; i++) {
+
+                            Console.Clear();
+
+                            Console.WriteLine("You are " + yearResults + " years, " + weeksInMonths + " weeks and " + daysResults3 + " days old " + DateTime.Now.Hour + " Hours " + DateTime.Now.Minute + " Min " + DateTime.Now.Second + " Sec ");
+                            Thread.Sleep(1000);
+                        }
+
+                        Console.WriteLine("Press e if you want to exit, Press c to continue see you age increasing, and press i to enter new valuse");
+                        string exitInput = Console.ReadLine();
+                        if (exitInput == "e") {
+                            exitCurrentSession = true;
+                            enterNewData = true;}
+                        else if (exitInput == "c") { exitCurrentSession = false; }
+                        else { exitCurrentSession = true;
+                            enterNewData = false; };
+
+                    }
                 }
-                else
-                {
-                    monthResults = todayMonth - birthMonth;
-                };
-
-
-
-                if (birthDay > todayDay)
-                {
-                    daysResults = 30 - (birthDay - todayDay);
-                    monthResults = monthResults - 1;
-                }
-                else
-                {
-                    daysResults = todayDay - birthDay;
-                };
-
-
-
-                int weeksFromDays = daysResults / 7;
-                int daysFromDays = daysResults % 7;
-
-               
-
-
-
-               double daysInMonths = monthResults * 30.41;
-               double leepDays = yearResults / 4;
-               double daysInMonth28 = daysInMonths + leepDays;
-
-               double reminingDaysinweeks = Math.Floor (daysInMonth28 % 7);
-               double daysResults3 = reminingDaysinweeks + daysFromDays;
-               double weeksInMonths = Math.Floor((daysInMonth28)/ 7+ weeksFromDays);
-
-
-
-
-
-                Console.WriteLine("You are " + yearResults + " years, " + weeksInMonths + " weeks and " + daysResults3 + " days old");
-                String inPut3 = Console.ReadLine();
-
-
             }
 
             catch (Exception e)
-            { Console.WriteLine("You entered some thing wrong "+ e.Message); Console.ReadLine(); };
-
-
-
-
+            { Console.WriteLine("You entered some thing wrong " + e.Message); Console.ReadLine(); };
 
         }
     
